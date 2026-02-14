8ab930efdfa46233f9289884c68652efbd636524
paolomolinariportela-create <paolomolinariportela@gmail.com>
2026-02-14 18:35:18 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AdminPanel from './pages/AdminPanel'; // Importando o único componente que importa
+
+// Importando as duas páginas
+import AdminPanel from './pages/AdminPanel'; 
+import Home from './pages/Home'; // <--- A nova página de login
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Qualquer link vai para o AdminPanel */}
-        <Route path="/" element={<AdminPanel />} />
+        {/* Rota Pública: Página de Login/Instalação */}
+        {/* Quem acessa 'seusite.com' cai aqui */}
+        <Route path="/" element={<Home />} />
+
+        {/* Rota Privada: Painel do Lojista */}
+        {/* Quem acessa 'seusite.com/admin' cai aqui (exige token) */}
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </BrowserRouter>
