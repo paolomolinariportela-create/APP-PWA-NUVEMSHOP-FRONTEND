eff15d9f3a52c998bfb5efa9fd6c8333ab3258fd
paolomolinariportela-create <paolomolinariportela@gmail.com>
2026-02-17 09:12:32 -0300

    Update AdminPanel.tsx

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -99,14 +99,14 @@ export default function AdminPanel() {
     ]).then(([dataConfig, dataStats, dataUrl]) => {
         setConfig({
              app_name: dataConfig.app_name ?? "Minha Loja",
-             theme_color: dataConfig.theme_color ?? "#000000",
-             logo_url: dataConfig.logo_url ?? "",
-             hatsapp_number: dataConfig.whatsapp_number ?? "",
-             fab_enabled: dataConfig.fab_enabled ?? false,
-             fab_text: dataConfig.fab_text ?? "Baixar App",
-             fab_position: dataConfig.fab_position ?? "right",
-             fab_icon: dataConfig.fab_icon ?? "📲",
-             fab_delay: dataConfig.fab_delay ?? 0
+            theme_color: dataConfig.theme_color ?? "#000000",
+            logo_url: dataConfig.logo_url ?? "",
+            whatsapp_number: dataConfig.whatsapp_number ?? "",
+            fab_enabled: dataConfig.fab_enabled ?? false,
+            fab_text: dataConfig.fab_text ?? "Baixar App",
+            fab_position: dataConfig.fab_position ?? "right",
+            fab_icon: dataConfig.fab_icon ?? "📲",
+            fab_delay: dataConfig.fab_delay ?? 0
         });
         setStats(dataStats);
         setStoreUrl(dataUrl.url);
