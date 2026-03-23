34b76b8f46641c3036b8aea6674753251bd366ea
paolomolinariportela-create <paolomolinariportela@gmail.com>
2026-03-23 13:42:08 -0300

    Create ConfigWidgets.tsx

---
+++ src/pages/ConfigWidgets.tsx
@@ -0,0 +1,9 @@
+import WidgetConfigPanel from '../app-config/WidgetConfigPanel';
+
+export default function ConfigWidgets() {
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <WidgetConfigPanel />
+    </div>
+  );
+}
